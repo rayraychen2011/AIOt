8256548d474e0c720d86867d10db08a8b1f76cf9
rayraychen2011 <rayraychen2011@gmail.com>
2025-11-23 11:03:24 +0800

    新增 LED 控制功能，實作 MQTT 訊息訂閱與處理，並調整 WiFi 連接與 GPIO 設定

--- class8.py/main.py
+++ class8.py/main.py
@@ -2,12 +2,27 @@ from umqtt.simple import MQTTClient
 import sys
 import time
 import mcu
+from machine import Pin, ADC
 
 
 def on_message(topic, msg):
+    global m
     msg = msg.decode("utf-8")
     topic = topic.decode("utf-8")
-    print(f"subscribe topic:{topic}, receive message:{msg}")
+    print(f"subscribe topic:{topic}, msg:{msg}")
+    m = msg
+
+
+def LED_ON():
+    Red.value(1)
+    Green.value(1)
+    Blue.value(1)
+
+
+def LED_OFF():
+    Red.value(0)
+    Green.value(0)
+    Blue.value(0)
 
 
 wi = mcu.wifi()
@@ -33,8 +48,28 @@ finally:
 mqClient0.set_callback(on_message)
 mqClient0.subscribe("Ray")
 
+gpio = mcu.gpio()
+Red = Pin(gpio.D5, Pin.OUT)
+Green = Pin(gpio.D6, Pin.OUT)
+Blue = Pin(gpio.D7, Pin.OUT)
+
+Red.value(0)
+Green.value(0)
+Blue.value(0)
+light_sensor = ADC(0)
+m = ""
 
 while True:
     mqClient0.check_msg()
     mqClient0.ping()
-    time.sleep(1)
+    light_sensor_value = light_sensor.read()
+    if m == "ON":
+        LED_ON()
+    elif m == "OFF":
+        LED_OFF()
+    elif m == "AUTO":
+        if light_sensor_value > 700:
+            LED_ON()
+        else:
+            LED_OFF()
+    time.sleep(0.1)
